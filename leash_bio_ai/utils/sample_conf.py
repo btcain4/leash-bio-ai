6ea48272bdfbe89e9e1b35240b5ac619958072c7
Brian Cain <btcain4@gmail.com>
2024-05-05 17:19:46 -0600

    feat(leash_bio_ai/utils/): added sample configuration file in utilities folder

---
+++ leash_bio_ai/utils/sample_conf.py
@@ -0,0 +1,4 @@
+# Data directories
+train_dir = ""  # Directory location of training data
+test_dir = ""  # Directory location of testing data
+sample_submission_dir = ""  # Directory location of sample submission
