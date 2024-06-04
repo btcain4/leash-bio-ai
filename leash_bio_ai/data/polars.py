b817e4ffdf58ee81b9ed71e162a3409049ea4276
Brian Cain <btcain4@gmail.com>
2024-06-03 19:56:12 -0600

    feat(leash_bio_ai/data/polars.py): added logger setup to abstract class for polars data pipelines

--- leash_bio_ai/data/polars.py
+++ leash_bio_ai/data/polars.py
@@ -1,16 +1,24 @@
 from abc import ABC, abstractmethod
 import polars as pl
+import logging
 
 
 class PolarsPipeline(ABC):
 
-    def __init__(self):
-        self.ldf = lazyframe()
-        self.logger = 0
+    def __init__(self, logger_file):
+        self.df = dataframe()
+        self.logger_file = logger_file
+        self.logger = setup_logger()
 
     @abstractmethod
-    def lazyframe(self):
+    def dataframe(self):
         pass
+
+    def setup_logger(self):
+        logger = logging.getLogger(__name__)
+        logging.basicConfig(filename=self.logger_file, level=logging.DEBUG)
+        logger.info("--- Logging Started ---")
+        return logger
 
     @abstractmethod
     def execute(self):
