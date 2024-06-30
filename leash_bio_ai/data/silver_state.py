ea5e5c4ba805ba5c86611337a0b5ffbe3b604dd2
Brian Cain <btcain4@gmail.com>
2024-06-29 19:32:37 -0600

    feat(leash_bio_ai/data/silver_state.py): created docstrings for methods and functions in script

--- leash_bio_ai/data/silver_state.py
+++ leash_bio_ai/data/silver_state.py
@@ -5,6 +5,17 @@ from leash_bio_ai.utils.conf import data_dir, bronze_train_dir, bronze_test_dir
 from leash_bio_ai.data.polars import PolarsPipeline, PipelineError
 
 
+def df_sampler(df, proportion):
+    """Returns a sample of a polars dataframes rows without replacement.
+
+    Args:
+        df (polars dataframe): dataframe to return a sample of.
+        proportion (float): number between 0 and 1 indicating the proportion of the dataset
+                            to be sampled.
+    """
+    pass
+
+
 class SilverPipeline(PolarsPipeline):
     """PolarsPipeline child class that setups and executes the data pipeline
        to transform the data from the bronze to the silver layers
@@ -25,12 +36,14 @@ class SilverPipeline(PolarsPipeline):
         return df
 
     def protein_imputation(self):
+        """Integer imputation on protein_name column to reduce dataset memory stress on local compute"""
         mapper = {"BRD4": 0, "HSA": 1, "sEH": 2}
         self.df = self.df.with_columns(
             protein_int=pl.col("protein_name").replace(mapper)
         )
 
     def column_correction(self):
+        """Column reduction and integer type correction to reduce dataset size and dataset memory stress on local compute"""
         self.df = self.df.select(
             "id",
             "molecule_smiles",
